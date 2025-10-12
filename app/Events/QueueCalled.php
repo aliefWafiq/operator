929f295a60fbb55eb94bcc3c6b7ff68af8476331
alief wafiq ramadhan <aliefwafiq2@gmail.com>
2025-10-12 17:19:32 +0700

    update

--- app/Events/QueueCalled.php
+++ app/Events/QueueCalled.php
@@ -4,11 +4,11 @@ namespace App\Events;
 
 use App\Models\antreans;
 use Illuminate\Broadcasting\Channel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class QueueCalled implements ShouldBroadcastNow
+class QueueCalled implements ShouldBroadcast
 {
     use Dispatchable, SerializesModels;
 
