22b0bf8087f8cec306b48b9987fc456513e61a46
alief wafiq ramadhan <aliefwafiq2@gmail.com>
2025-10-16 02:24:06 +0700

    update

--- app/Models/antreans.php
+++ app/Models/antreans.php
@@ -18,6 +18,7 @@ class antreans extends Authenticatable
      */
     protected $fillable = [
         'namaLengkap',
+        'email',
         'nomorHp',
         'password',
         'noPerkara',
